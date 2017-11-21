6cd8d673181b7f45bf82891ae63fc1f51853efee
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-21 12:39:17 -0800

    getting random emoji

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -48,11 +48,16 @@ class ViewController: UIViewController {
     
     var emojiChoices = ["👻","🎃","😈","🙀","🦇","🍭","🍎","🍬","🍫"]
     
+    var emoji = [Int:String]()
+    
     func emoji(for card: Card) -> String {
-        return "?"
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            }
+        
+        return emoji[card.identifier] ?? "?"
     }
-    
-    
 }
 
 
