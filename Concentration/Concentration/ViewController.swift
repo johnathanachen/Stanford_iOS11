bdde88321545776187e83ef555c16849337f2a0f
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-21 06:54:04 -0800

    UI update

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var flipCount: Int = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(withEmoji: "👻", on: sender)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(withEmoji: "🎃", on: sender)
     }
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
 
 }
 
