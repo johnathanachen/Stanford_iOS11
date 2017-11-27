95a106f58948e5ad4b7339bf6e81e7fb92e57193
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-27 06:47:01 -0800

    bezierpath

--- creatingViews/creatingViews/PlayingCardView.swift
+++ creatingViews/creatingViews/PlayingCardView.swift
@@ -11,15 +11,32 @@ import UIKit
 class PlayingCardView: UIView {
     
     override func draw(_ rect: CGRect) {
+        // Option 1 of drawing
+        /*
         if let context = UIGraphicsGetCurrentContext() {
             context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-            
+
             context.setLineWidth(5.0)
             UIColor.green.setFill()
             UIColor.red.setStroke()
             context.strokePath()
             context.fillPath()
         }
+         */
+        
+        // Draw option 2
+        /*
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.green.setFill()
+        UIColor.red.setStroke()
+        path.stroke()
+        path.fill()
+         */
+ 
+        // Drawing a card
+        
     }
 
 }
