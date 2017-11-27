6f775768f0b67659011f686ae4ccb461a63eae34
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-26 17:10:34 -0800

    custom string convert

--- creatingViews/creatingViews/PlayingCard.swift
+++ creatingViews/creatingViews/PlayingCard.swift
@@ -8,19 +8,64 @@
 
 import Foundation
 
-struct PlayingCard {
+struct PlayingCard: CustomStringConvertible {
+    
+    var description: String { return "\(rank)\(suit)" }
+    
     var suit: Suit
     var rank: Rank
     
-    enum Suit: String {
+    enum Suit: String, CustomStringConvertible {
+        var description: String
+        
         case spades = "♠️"
         case hearts = "♥️"
         case diamonds = "♦️"
         case clubs = "♣️"
+        
+        static var all = [Suit.spades,.hearts,.diamonds,.clubs]
     }
     
-    enum Rank {
+    enum Rank: CustomStringConvertible {
+        var description: String
+        
         case ace
         case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+            case .face(let kind) where kind == "Q": return 12
+            case .face(let kind) where kind == "K": return 13
+            default: return 0
+            }
+        }
+        
+        static var all: [Rank] {
+            var allRanks: [Rank] = [Rank.ace]
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+            }
+            allRanks += [Rank.face("J"),face("Q"),face("K")]
+            return allRanks
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- creatingViews/creatingViews/ViewController.swift
+++ creatingViews/creatingViews/ViewController.swift
@@ -11,12 +11,16 @@ import CoreGraphics
 
 class ViewController: UIViewController {
 
+    var deck = PlayingCardDeck()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
     }
-    
-
 
 }
 
