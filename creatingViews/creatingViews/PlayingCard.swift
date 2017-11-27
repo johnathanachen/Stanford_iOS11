493e824494af189d8182f537438bd1375d9e2485
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-27 06:31:22 -0800

    Playing card view

--- creatingViews/creatingViews/PlayingCard.swift
+++ creatingViews/creatingViews/PlayingCard.swift
@@ -16,7 +16,6 @@ struct PlayingCard: CustomStringConvertible {
     var rank: Rank
     
     enum Suit: String, CustomStringConvertible {
-        var description: String
         
         case spades = "♠️"
         case hearts = "♥️"
@@ -24,10 +23,11 @@ struct PlayingCard: CustomStringConvertible {
         case clubs = "♣️"
         
         static var all = [Suit.spades,.hearts,.diamonds,.clubs]
+        
+        var description: String { return rawValue }
     }
     
     enum Rank: CustomStringConvertible {
-        var description: String
         
         case ace
         case face(String)
@@ -51,6 +51,14 @@ struct PlayingCard: CustomStringConvertible {
             }
             allRanks += [Rank.face("J"),face("Q"),face("K")]
             return allRanks
+        }
+        
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .numeric(let pips): return String(pips)
+            case .face(let kind): return kind
+            }
         }
     }
 }
---
+++ creatingViews/creatingViews/PlayingCardView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayingCardView.swift
+//  creatingViews
+//
+//  Created by Johnathan Chen on 11/27/17.
+//  Copyright © 2017 JCSwifty. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
